8ffda954d21c12e490eeb79cf18a88090f3e6cb4
Fabian Grutschus <f.grutschus@lubyte.de>
2015-11-24 14:41:50 +0100

    Removed trimming of JS file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,14 @@
 var gulp = require('gulp');
 var closureCompiler = require('closure-compiler-stream');
 var sourcemaps = require('gulp-sourcemaps');
-var trim = require('gulp-trim');
 
 gulp.task('compile', function () {
     return gulp.src('src/*.js')
         .pipe(sourcemaps.init())
         .pipe(closureCompiler({
             jar: 'bower_components/closure-compiler/compiler.jar',
-            js_output_file: 'ErrorHandler.min.js'
+            js_output_file: 'dist/ErrorHandler.min.js'
         }))
-        .pipe(trim())
         .pipe(sourcemaps.write('maps', { sourceRoot: "src/" }))
         .pipe(gulp.dest('dist'));
 });
