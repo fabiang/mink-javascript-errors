412ef9feea69d61934b9bfacdeef14887998cd02
Fabian Grutschus <f.grutschus@lubyte.de>
2015-11-24 14:32:57 +0100

    Added source map file for minified JavaScript file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
-var closureCompiler = require('gulp-closure-compiler');
+var closureCompiler = require('closure-compiler-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var trim = require('gulp-trim');
 
 gulp.task('compile', function () {
     return gulp.src('src/*.js')
+        .pipe(sourcemaps.init())
         .pipe(closureCompiler({
-            compilerPath: 'bower_components/closure-compiler/compiler.jar',
-            fileName: 'ErrorHandler.min.js'
+            jar: 'bower_components/closure-compiler/compiler.jar',
+            js_output_file: 'ErrorHandler.min.js'
         }))
+        .pipe(trim())
+        .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('dist'));
 });
